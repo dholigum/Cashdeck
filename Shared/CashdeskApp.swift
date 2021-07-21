3f33cf35c9de39f2cc43f147ef29bfd0a6313f08
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-07-21 09:47:50 +0700

    Initial Commit

---
+++ Shared/CashdeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashdeskApp.swift
+//  Shared
+//
+//  Created by Syahrul Apple Developer BINUS on 21/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CashdeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
