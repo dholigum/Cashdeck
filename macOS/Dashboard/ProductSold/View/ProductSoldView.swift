c753c6144b9c55f3c2ee0ea87d4cbca029bb92a2
revarinoo <revarinoputra@gmail.com>
2021-08-03 23:14:49 +0700

    Add Product Sold View and Font

--- macOS/Dashboard/ProductSold/View/ProductSoldView.swift
+++ macOS/Dashboard/ProductSold/View/ProductSoldView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct ProductSoldView: View {
     var body: some View {
-        VStack {
+        VStack (alignment: .leading, spacing: 24) {
             Text("Product Sold")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .font(Font.custom("SFProDisplay-Bold", size: 24))
+                .foregroundColor(Color("AccentColor2"))
+                
+            
+            HStack (spacing: 24) {
+                TotalNetIncome()
+                TotalSold()
+            }
         }
+        .padding(EdgeInsets(top: 45, leading: 12, bottom: 0, trailing: 0))
     }
 }
 
