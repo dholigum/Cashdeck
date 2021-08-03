cc38166b031e3cf313d34e5b488b53bc674eedca
revarinoo <revarinoputra@gmail.com>
2021-08-03 22:31:09 +0700

    Fix setup

---
+++ macOS/Dashboard/ProductSold/View/ProductSoldView.swift
@@ -0,0 +1,23 @@
+//
+//  ProductSoldView.swift
+//  Cashdeck
+//
+//  Created by Revarino Putra on 03/08/21.
+//
+
+import SwiftUI
+
+struct ProductSoldView: View {
+    var body: some View {
+        VStack {
+            Text("Product Sold")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ProductSoldView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductSoldView()
+    }
+}
