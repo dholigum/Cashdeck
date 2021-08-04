87b58f87b75228f654414b617e71e05d8f9797ae
revarinoo <revarinoputra@gmail.com>
2021-08-04 12:18:35 +0700

    Modify ProductSoldModel

--- macOS/Dashboard/ProductSold/Model/ProductSoldModel.swift
+++ macOS/Dashboard/ProductSold/Model/ProductSoldModel.swift
@@ -1,18 +1,37 @@
 //
 //  ProductSoldModel.swift
-//  Cashdeck (iOS)
+//  Cashdeck
 //
 //  Created by Revarino Putra on 04/08/21.
 //
 
 import Foundation
+import CoreData
 
 class ProductSoldModel {
-    
     static let shared = ProductSoldModel()
     let context = CoreDataManager.sharedManager.persistentContainer.viewContext
     
-    func insertData() {
+    func insertProduct(name: String, SKU: String, color: String, size: String, costPrice: Int) {
+        let newProduct = Products(context: self.context)
+        newProduct.sku = SKU
+        newProduct.name = name
+        newProduct.color = color
+        newProduct.size = color
+        newProduct.costPrice = Int64(costPrice)
+        CoreDataManager.sharedManager.saveContext()
+    }
+    
+    func insertTransactionDetail(price: Int64, qty: Int64) {
+        let newTransactionDetail = TransactionDetail(context: self.context)
+        newTransactionDetail.price = price
+        newTransactionDetail.quantity = qty
         
+//        newTransactionDetail.td_product =
+        
+    }
+    
+    func connectTDProduct(td: TransactionDetail, product: Products) {
+        td.td_product = product
     }
 }
