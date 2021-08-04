81738a3138aef744a504563664c87e529f671b5a
revarinoo <revarinoputra@gmail.com>
2021-08-04 11:17:39 +0700

    Add Product Sold Model

---
+++ macOS/Dashboard/ProductSold/Model/ProductSoldModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductSoldModel.swift
+//  Cashdeck (iOS)
+//
+//  Created by Revarino Putra on 04/08/21.
+//
+
+import Foundation
+
+class ProductSoldModel {
+    
+    static let shared = ProductSoldModel()
+    let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+    
+    func insertData() {
+        
+    }
+}
--- macOS/Dashboard/ProductSold/View/Component/ProductSoldList.swift
+++ macOS/Dashboard/ProductSold/View/Component/ProductSoldList.swift
@@ -14,20 +14,32 @@ struct ProductSoldList: View {
                 .font(Font.custom("SFProDisplay-Semibold", size: 18))
                 .foregroundColor(Color("AccentColor2"))
             
-            HStack (spacing: 100) {
+            HStack {
                 Text("SKU")
                     .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                    .frame(width: 150, alignment: .leading)
+                
                 Text("Name")
                     .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                    .frame(width: 330, alignment: .leading)
+                
                 Text("Colour")
                     .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                    .frame(width: 105, alignment: .leading)
+                
                 Text("Size")
                     .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                    .frame(width: 90, alignment: .leading)
+                
                 Text("Quantity")
                     .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                    .frame(width: 118, alignment: .leading)
+                
                 Text("Net Income")
                     .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                    .frame(width: 120, alignment: .leading)
             }
+            
         }.frame(width: 1009, height: 650.94)
     }
 }
