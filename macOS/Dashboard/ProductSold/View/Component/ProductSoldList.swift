786136c4e03bd028c6df2d77432ae150f0d5dd43
revarinoo <revarinoputra@gmail.com>
2021-08-04 10:31:20 +0700

    Create Product Sold Table

--- macOS/Dashboard/ProductSold/View/Component/ProductSoldList.swift
+++ macOS/Dashboard/ProductSold/View/Component/ProductSoldList.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 struct ProductSoldList: View {
     var body: some View {
-        VStack {
+        VStack (alignment: .leading) {
+            Text("Best Seller")
+                .font(Font.custom("SFProDisplay-Semibold", size: 18))
+                .foregroundColor(Color("AccentColor2"))
             
-        }
+            HStack (spacing: 100) {
+                Text("SKU")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                Text("Name")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                Text("Colour")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                Text("Size")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                Text("Quantity")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 16))
+                Text("Net Income")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 16))
+            }
+        }.frame(width: 1009, height: 650.94)
     }
 }
 
