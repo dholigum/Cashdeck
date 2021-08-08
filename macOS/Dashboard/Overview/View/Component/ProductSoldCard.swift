dd5116c9da84dc1dc5a3717c4cab97e4c78f640e
revarinoo <revarinoputra@gmail.com>
2021-08-08 22:21:31 +0700

    Modify home overview

--- macOS/Dashboard/Overview/View/Component/ProductSoldCard.swift
+++ macOS/Dashboard/Overview/View/Component/ProductSoldCard.swift
@@ -8,21 +8,72 @@
 import SwiftUI
 
 struct ProductSoldCard: View {
+    let productSoldVM = ProductSoldViewModel()
+    
     var body: some View {
         VStack {
-            Text("ProductSoldCard")
-                .font(Font.title.weight(.semibold))
-                .padding(.vertical)
-                .foregroundColor(Color("AccentColor2"))
-                .padding(.top, 4)
-                .padding(.leading, 20)
+            GeometryReader { geometry in
+                Text("Product Sold")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 18))
+                    .foregroundColor(Color("AccentColor2"))
+                    .frame(width: geometry.frame(in: .local).size.width, alignment: .leading)
+                    .padding(.leading, 25)
+            }.frame(height: 24)
+            
+            
+            
+            HStack {
+                Text("SKU")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 14))
+                    .frame(width: 100, alignment: .leading)
+                
+                Text("Quantity")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 14))
+                    .frame(minWidth: 72, alignment: .leading)
+                
+                Text("Color")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 14))
+                    .frame(minWidth: 82, alignment: .leading)
+                
+                Text("Size")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 14))
+                    .frame(minWidth: 77, alignment: .leading)
+                
+                Text("Net Income")
+                    .font(Font.custom("SFProDisplay-Semibold", size: 14))
+                    .frame(minWidth: 30, alignment: .leading)
+            }
+            
+            ScrollView {
+                ForEach(self.productSoldVM.productSold, id: \.self) { data in
+                    VStack {
+                        HStack {
+                            Text(data.sku)
+                                .frame(width: 100, alignment: .leading)
+                                    .font(Font.custom("SFProDisplay-Regular", size: 12))
+                            Text("\(data.quantity)")
+                                    .frame(minWidth: 72, alignment: .leading)
+                                    .font(Font.custom("SFProDisplay-Regular", size: 12))
+                            Text(data.color)
+                                    .frame(minWidth: 82, alignment: .leading)
+                                    .font(Font.custom("SFProDisplay-Regular", size: 14))
+                            Text(data.size)
+                                    .frame(minWidth: 77, alignment: .leading)
+                                    .font(Font.custom("SFProDisplay-Regular", size: 14))
+                            Text("+Rp \(data.netIncome)")
+                                    .frame(minWidth: 30, alignment: .leading)
+                                    .font(Font.custom("SFProDisplay-Regular", size: 14))
+                        }
+                        line
+                    }
+                }
+            }
         }
-        .frame(width: 500, height: 380)
+        .frame(width: 490, height: 380)
         .background(Color.white)
-        .cornerRadius(16)
+        .cornerRadius(15)
         .clipped()
         .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 4, x: 2, y: 2)
-        .padding(.horizontal, 2)
     }
 }
 
