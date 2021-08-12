0265ee0acdf2c48b31b8884f9dbe32eec4e230b2
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-08-12 23:34:45 +0700

    Handle no expenses illustration

--- macOS/Expense/Overview/View/Component/RecentExpenseOverviewCard.swift
+++ macOS/Expense/Overview/View/Component/RecentExpenseOverviewCard.swift
@@ -23,15 +23,15 @@ struct RecentExpenseOverviewCard: View {
             CustomDivider(width: 560)
                 .padding(EdgeInsets(top: -8, leading: 16, bottom: 0, trailing: 0))
             
-            if expenseVM.expenses.count > 0 {
-                ScrollView(.vertical, showsIndicators: true, content: {
-                    LazyVStack {
+            ScrollView(.vertical, showsIndicators: true, content: {
+                LazyVStack {
+                    if expenseVM.expenses.count > 0 {
                         ForEach(expenseVM.expenses, id: \.self) { expense in
                             VStack(alignment: .leading) {
                                 HStack {
-
+                                    
                                     if let date = expense.date, let category = expense.category, let name = expense.name, let price = expense.price {
-
+                                        
                                         Text(date.shortDateFormatting())
                                             .font(.system(size: 14))
                                             .frame(width: 80, alignment: .leading)
@@ -50,23 +50,24 @@ struct RecentExpenseOverviewCard: View {
                                 CustomDivider(width: 560)
                             }
                         }
+                    } else {
+                        VStack(alignment: .center) {
+                            
+                            Image("noExpensesIllustration")
+                                .resizable()
+                                .frame(width: 178, height: 136)
+                                .aspectRatio(contentMode: .fill)
+                            
+                            Text("You have no expenses yet.")
+                                .font(.system(size: 16))
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color("AccentColor2"))
+                        }
+                        .padding(.top, 36)
                     }
-                })
-                .padding(EdgeInsets(top: 0, leading: -8, bottom: 0, trailing: 0))
-            } else {
-                HStack {
-                    Image("noTransactionIllustration")
-                        .resizable()
-                        .frame(width: 356, height: 272)
-                        .aspectRatio(contentMode: .fill)
-                } .padding(.top, 30)
-                
-                Text("You have no expenses yet.")
-                    .font(.system(size: 24))
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color("AccentColor2"))
-            }
-            
+                }
+            })
+            .padding(EdgeInsets(top: 0, leading: -8, bottom: 0, trailing: 0))
             
         }
         .frame(width: 600, height: 380)
