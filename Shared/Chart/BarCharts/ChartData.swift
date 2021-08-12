e9e1a0aa0aca54bdec683ff255b88ff2b4b434f9
Shiddiq <syuhadashiddiq97@gmail.com>
2021-08-12 12:53:33 +0700

    test negative value bar-chart

--- Shared/Chart/BarCharts/ChartData.swift
+++ Shared/Chart/BarCharts/ChartData.swift
@@ -18,7 +18,7 @@ let chartDataSet =
          ChartData(label: "January 2021", value: 340.32, day: "Monday"),
          ChartData(label: "February 2021", value: 250.0, day: "Tuesday"),
          ChartData(label: "March 2021", value: 430.22, day: "Wednesday"),
-         ChartData(label: "April 2021", value: 350.0, day: "Thursday"),
+         ChartData(label: "April 2021", value: -350.0, day: "Thursday"),
          ChartData(label: "May 2021", value: 450.0, day: "Friday"),
          ChartData(label: "June 2021", value: 380.0, day: "Saturday"),
          ChartData(label: "July 2021", value: 900.98, day: "Sunday")
