618c6fe561d46168205004549c6f66c34d5f4e0a
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-08-13 21:56:50 +0700

    Add calendar view

---
+++ macOS/Income/Component/TransactionByMonthCalendar.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionByMonthCalendar.swift
+//  Cashdeck
+//
+//  Created by Syahrul Apple Developer BINUS on 13/08/21.
+//
+
+import SwiftUI
+
+struct TransactionByMonthCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionByMonthCalendar_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionByMonthCalendar()
+    }
+}
